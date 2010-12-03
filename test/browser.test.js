4fee9a6c9fc5c25ef2e5de413b9c1c27c7fda308
Tj Holowaychuk <tj@vision-media.ca>
2010-12-02 23:29:15 -0800

    Moved some tests to navigation file

--- test/browser.test.js
+++ test/browser.test.js
@@ -8,17 +8,7 @@ var tobi = require('tobi')
   , Browser = tobi.Browser
   , should = require('should');
 
-// Test app
-
 var app = express.createServer();
-
-app.get('/', function(req, res){
-  res.send('<p>Hello World</p>');
-});
-
-app.get('/user/:id', function(req, res){
-  res.send('<h1>Tobi</h1><p>the ferret</p>');
-});
 
 module.exports = {
   'test Browser(str)': function(){
@@ -38,42 +28,5 @@ module.exports = {
   'test .createBrowser(server)': function(){
     var browser = tobi.createBrowser(app);
     browser.should.have.property('server', app);
-  },
-  
-  'test .request(method, path)': function(done){
-    var browser = tobi.createBrowser(app);
-    browser.request('GET', '/', function($){
-      $.should.equal(browser.jQuery);
-      browser.should.have.property('path', '/');
-      browser.history.should.eql(['/']);
-      browser.request('GET', '/user/0', function(){
-        browser.should.have.property('path', '/user/0');
-        browser.history.should.eql(['/', '/user/0']);
-        browser.should.have.property('source', '<h1>Tobi</h1><p>the ferret</p>');
-        browser.jQuery('p').text().should.equal('the ferret');
-        done();
-      });
-    });
-  },
-  
-  'test .back(fn)': function(done){
-    var browser = tobi.createBrowser(app);
-    browser.request('GET', '/', function($){
-      browser.request('GET', '/user/0', function(){
-        browser.back(function(){
-          browser.should.have.property('path', '/');
-          done();
-        });
-      });
-    });
-  },
-  
-  'test .get(path)': function(done){
-    var browser = tobi.createBrowser(app);
-    browser.get('/', function(){
-      browser.should.have.property('path', '/');
-      browser.history.should.eql(['/']);
-      done();
-    });
   }
 };

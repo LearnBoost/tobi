546387624e5feabc1c74d6c07465b19b007ec1bf
Tj Holowaychuk <tj@vision-media.ca>
2010-12-02 16:25:05 -0800

    Added Browser

---
+++ test/browser.test.js
@@ -0,0 +1,18 @@
+
+/**
+ * Module dependencies.
+ */
+
+var chrono = require('chrono')
+  , Browser = chrono.Browser
+  , should = require('should');
+
+module.exports = {
+  'test Browser(str)': function(){
+    var html = '<ul><li>one</li><li>two</li></ul>'
+      , browser = new Browser(html);
+    browser.should.have.property('source', html);
+    browser.should.have.property('window');
+    browser.should.have.property('jQuery');
+  }
+};

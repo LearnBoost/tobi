372ac320186443a608e5981c5028192555f4342f
Tj Holowaychuk <tj@vision-media.ca>
2010-12-02 16:27:19 -0800

    Added .createBrowser()

--- test/browser.test.js
+++ test/browser.test.js
@@ -14,5 +14,11 @@ module.exports = {
     browser.should.have.property('source', html);
     browser.should.have.property('window');
     browser.should.have.property('jQuery');
+  },
+  
+  'test .createBrowser(str)': function(){
+    var html = '<ul><li>one</li><li>two</li></ul>'
+      , browser = chrono.createBrowser(html);
+    browser.should.have.property('source', html);
   }
 };

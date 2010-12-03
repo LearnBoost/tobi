2949cd7623c47288d6c25dd0e465c0b6176324ef
Tj Holowaychuk <tj@vision-media.ca>
2010-12-03 15:39:21 -0800

    Added jQuery#submit() support

--- test/browser.navigation.test.js
+++ test/browser.navigation.test.js
@@ -310,5 +310,23 @@ module.exports = {
         done();
       });
     });
+  },
+  
+  'test jQuery#submit()': function(done){
+    var browser = tobi.createBrowser(app);
+    browser.get('/form', function($){
+      $('[name=user[name]]').val('tjholowaychuk');
+      $('#signature').val('Wahoo');
+       $('form').submit(function(res){
+        res.body.headers.should.have.property('content-type', 'application/x-www-form-urlencoded');
+        res.body.body.should.eql({
+          user: {
+              name: 'tjholowaychuk'
+            , signature: 'Wahoo'
+          }
+        });
+        done();
+      });
+    });
   }
 };

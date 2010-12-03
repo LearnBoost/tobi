3ded82524e166db5ff12630f4df5871df6744466
Tj Holowaychuk <tj@vision-media.ca>
2010-12-02 19:56:06 -0800

    Added .many()

--- test/assertions.test.js
+++ test/assertions.test.js
@@ -29,29 +29,45 @@ app.get('/user/:id', function(req, res){
   res.send('<h1 id="title">Tobi</h1><p>the ferret</p>');
 });
 
-module.exports = {
-  'test .text()': function(done){
-    browser.get('/user/1', function($){
-      $('p').prev().should.have.text('Tobi');
-      $('p').prev().should.have.text(/^To/);
-      
-      err(function(){
-        $('h1').should.not.have.text('Tobi');
-      }, "expected [jQuery 'h1'] to not have text 'Tobi'");
+app.get('/list', function(req, res){
+  res.send('<ul><li>one</li><li>two</li><li>three</li></ul>');
+});
 
-      err(function(){
-        $('h1').should.have.text('Raul');
-      }, "expected [jQuery 'h1'] to have text 'Raul'");
-      
-      err(function(){
-        $('h1').should.have.text(/^Raul/);
-      }, "expected [jQuery 'h1'] to have text matching /^Raul/");
-      
-      err(function(){
-        $('h1').should.not.have.text(/^To/);
-      }, "expected [jQuery 'h1'] text 'Tobi' to not match /^To/");
+// Tests
 
-      done();
-    });
-  }
+exports['test .text()'] = function(done){
+  browser.get('/user/1', function($){
+    $('p').prev().should.have.text('Tobi');
+    $('p').prev().should.have.text(/^To/);
+    
+    err(function(){
+      $('h1').should.not.have.text('Tobi');
+    }, "expected [jQuery 'h1'] to not have text 'Tobi'");
+
+    err(function(){
+      $('h1').should.have.text('Raul');
+    }, "expected [jQuery 'h1'] to have text 'Raul'");
+    
+    err(function(){
+      $('h1').should.have.text(/^Raul/);
+    }, "expected [jQuery 'h1'] to have text matching /^Raul/");
+    
+    err(function(){
+      $('h1').should.not.have.text(/^To/);
+    }, "expected [jQuery 'h1'] text 'Tobi' to not match /^To/");
+
+    done();
+  });
+};
+
+exports['test .many()'] = function(done){
+  browser.get('/list', function($){
+    $('ul').should.have.many('li');
+    
+    err(function(){
+      $('ul').should.have.many('p');
+    }, "expected [jQuery 'ul'] to have many 'p' tags, but has none");
+    
+    done();
+  });
 };

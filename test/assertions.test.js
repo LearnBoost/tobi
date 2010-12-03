c4b6536f4ef313cb6defbe3352eb739dfb3f1502
Tj Holowaychuk <tj@vision-media.ca>
2010-12-02 18:59:19 -0800

    Added err() assertion helper because the node assert module sucks

--- test/assertions.test.js
+++ test/assertions.test.js
@@ -7,6 +7,19 @@ var tobi = require('tobi')
   , express = require('express')
   , should = require('should');
 
+// Assert error
+
+function err(fn, msg){
+  var err;
+  try {
+    fn();
+  } catch (e) {
+    should.equal(e, msg);
+    return;
+  }
+  throw new Error('no exception thrown, expected "' + msg + '"');
+}
+
 // Test app
 
 var app = express.createServer()
@@ -17,10 +30,19 @@ app.get('/user/:id', function(req, res){
 });
 
 module.exports = {
-  'test ': function(done){
+  'test .text()': function(done){
     browser.get('/user/1', function($){
       $('p').prev().should.have.text('Tobi');
       $('p').prev().should.have.text(/^To/);
+      
+      err(function(){
+        $('p').prev().should.not.have.text('Tobi');
+      }, "expected [jQuery 'p'] to not have text 'Tobi'");
+
+      err(function(){
+        $('p').prev().should.have.text('Raul');
+      }, "expected [jQuery 'p'] to have text 'Raul'");
+
       done();
     });
   }

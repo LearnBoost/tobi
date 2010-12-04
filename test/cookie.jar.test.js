22fa5f00a37ff04c5078ffa42727f47b30a81e54
Tj Holowaychuk <tj@vision-media.ca>
2010-12-04 12:36:25 -0800

    Added another CookieJar test

--- test/cookie.jar.test.js
+++ test/cookie.jar.test.js
@@ -14,9 +14,8 @@ function expires(ms) {
 
 module.exports = {
   'test .get() expiration': function(done){
-    var str = 'sid=1234; path=/; expires=' + expires(1000);
-    var cookie = new Cookie(str);
     var jar = new Jar;
+    var cookie = new Cookie('sid=1234; path=/; expires=' + expires(1000));
     jar.add(cookie);
     setTimeout(function(){
       var cookies = jar.get({ url: 'http://foo.com/foo' });
@@ -28,5 +27,23 @@ module.exports = {
         done();
       }, 1000);
     }, 5);
+  },
+  
+  'test .get() path support': function(){
+    var jar = new Jar;
+    var a = new Cookie('sid=1234; path=/');
+    var b = new Cookie('sid=1111; path=/foo/bar');
+    var c = new Cookie('sid=2222; path=/');
+    jar.add(a);
+    jar.add(b);
+    jar.add(c);
+
+    var cookies = jar.get({ url: 'http://foo.com/' });
+    cookies.should.have.length(2);
+    cookies[0].should.equal(a);
+    cookies[1].should.equal(c);
+
+    var cookies = jar.get({ url: 'http://foo.com/foo/bar' });
+    cookies.should.have.length(3);
   }
 };

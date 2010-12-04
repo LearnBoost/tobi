8d36519f053c4ffda80818d6357a8a171b1d2464
Tj Holowaychuk <tj@vision-media.ca>
2010-12-04 12:25:31 -0800

    Started CookieJar implementation

--- test/cookie.jar.test.js
+++ test/cookie.jar.test.js
@@ -8,8 +8,25 @@ var tobi = require('tobi')
   , Jar = tobi.CookieJar
   , should = require('should');
 
+function expires(seconds) {
+  return new Date(Date.now() + (seconds * 1000)).toUTCString();
+}
+
 module.exports = {
-  'test': function(){
-    
+  'test .get() expiration': function(done){
+    var str = 'sid=1234; path=/; expires=' + expires(15);
+    var cookie = new Cookie(str);
+    var jar = new Jar;
+    jar.add(cookie);
+    setTimeout(function(){
+      var cookies = jar.get({ url: 'http://foo.com/foo' });
+      cookies.should.have.length(1);
+      cookies[0].should.equal(cookie);
+      setTimeout(function(){
+        var cookies = jar.get({ url: 'http://foo.com/foo' });
+        cookies.should.have.length(0);
+        done();
+      }, 25);
+    }, 5);
   }
 };

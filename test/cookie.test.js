611106db4cf0de525e7b84ad7b6c2f6d74c930b3
Tj Holowaychuk <tj@vision-media.ca>
2010-12-04 11:41:31 -0800

    Added Cookie tests

--- test/cookie.test.js
+++ test/cookie.test.js
@@ -5,10 +5,30 @@
 
 var tobi = require('tobi')
   , Cookie = tobi.Cookie
+  , Jar = tobi.CookieJar
   , should = require('should');
 
+var str = 'connect.sid=s543qactge.wKE61E01Bs%2BKhzmxrwrnug; path=/; httpOnly; expires=Sat, 04 Dec 2010 23:27:28 GMT';
+var cookie = new Cookie(str);
+
 module.exports = {
-  'test .version': function(){
-    tobi.version.should.match(/^\d+\.\d+\.\d+$/);
+  'test .toString()': function(){
+    cookie.toString().should.equal(str);
+  },
+  
+  'test .path': function(){
+    cookie.should.have.property('path', '/');
+  },
+  
+  'test .httpOnly': function(){
+    cookie.should.have.property('httpOnly', true);
+  },
+  
+  'test .name': function(){
+    cookie.should.have.property('name', 'connect.sid');
+  },
+  
+  'test .value': function(){
+    cookie.should.have.property('value', 's543qactge.wKE61E01Bs%2BKhzmxrwrnug');
   }
 };

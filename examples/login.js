4a219e187d51a9850f701c7b38f03fa9f2fd3ae7
Tj Holowaychuk <tj@vision-media.ca>
2010-12-05 08:18:38 -0800

    Added value() / method() / action() assertion methods

--- examples/login.js
+++ examples/login.js
@@ -12,6 +12,17 @@ var app = require('./app')
   , should = require('should')
   , browser = tobi.createBrowser(app);
 
+browser.get('/login', function($){
+  $('form')
+    .should.have.action('/login')
+    .and.have.method('post')
+    .and.have.many('input');
+  
+  $('form > input[name=username]').should.have.attr('type', 'text');
+  $('form > input[name=password]').should.have.attr('type', 'password');
+  $('form :submit').should.have.value('Login');
+});
+
 browser.get('/login', function(){
   browser.fill({
       username: 'tj'
@@ -19,5 +30,6 @@ browser.get('/login', function(){
   }).click('Login', function($, res){
     res.statusCode.should.equal(200)
     $('ul.messages').should.have.one('li', 'Successfully authenticated');
+    console.log('Testing complete');
   })
 });

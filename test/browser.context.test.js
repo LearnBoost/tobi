e56927b779aaaa0ef9735cd289ba591f4c6c190f
Tj Holowaychuk <tj@vision-media.ca>
2010-12-07 13:32:52 -0800

    Added forms for new context tests

--- test/browser.context.test.js
+++ test/browser.context.test.js
@@ -14,28 +14,22 @@ var app = express.createServer();
 
 app.use(express.bodyDecoder());
 
-app.get('/form', function(req, res){
-  res.send('<form id="user" method="post">'
-    + '<input id="user-name" type="text" name="user[name]" />'
-    + '<input id="user-email" type="text" name="user[email]" disabled="disabled" />'
-    + '<input type="checkbox" name="user[agreement]" id="user-agreement" value="yes" />'
-    + '<input type="checkbox" name="user[subscribe]" checked="checked" value="yes" />'
-    + '<input type="submit" value="Update" />'
-    + '<fieldset>'
-    + '  <select name="user[forum_digest]">'
-    + '    <option value="none">None</option>'
-    + '    <option value="daily">Once per day</option>'
-    + '    <option value="weekly">Once per week</option>'
-    + '  </select>'
-    + '  <textarea id="signature" name="user[signature]"></textarea>'
-    + '  <input type="radio" name="user[display_signature]" value="Yes" />'
-    + '  <input type="radio" name="user[display_signature]" value="No" />'
-    + '</fieldset>'
+app.get('/search', function(req, res){
+  res.send(
+      '<form id="user-search" action="/search/users">'
+    + '  <input type="text" name="query" />'
+    + '</form>'
+    + '<form id="post-search" action="/search/posts">'
+    + '  <input type="text" name="query" />'
     + '</form>');
 });
 
-app.post('/form', function(req, res){
-  res.send({ headers: req.headers, body: req.body });
+app.post('/search/users', function(req, res){
+  res.send({ users: true, headers: req.headers, body: req.body });
+});
+
+app.post('/search/posts', function(req, res){
+  res.send({ posts: true, headers: req.headers, body: req.body });
 });
 
 module.exports = {

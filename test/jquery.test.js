47c6e31d6826d6ec3adcbc5a7be947612630b864
Tj Holowaychuk <tj@vision-media.ca>
2010-12-07 11:06:43 -0800

    Added more jQuery#fill() tests

--- test/jquery.test.js
+++ test/jquery.test.js
@@ -14,9 +14,23 @@ module.exports = {
   'test .fill()': function(){
     var $ = jquery('<form>'
       + '<input type="text" name="username" />'
+      + '<input type="password" name="password" />'
+      + '<input type="radio" name="signature" value="display" />'
+      + '<input type="radio" name="signature" value="hide" />'
+      + '<input type="checkbox" name="agreement" value="ageed" />'
+      + '<input type="checkbox" name="email" value="yes" checked="checked" />'
       + '</form>');
 
-    $('form').fill({ username: 'tjholowaychuk' });
+    $('form').fill({
+        username: 'tjholowaychuk'
+      , signature: 'display'
+      , agreement: true
+      , email: false
+    });
+
     $('form > input[name=username]').should.have.attr('value', 'tjholowaychuk');
+    $('form > input[value=display]').should.be.checked;
+    $('form > input[name=agreement]').should.be.checked;
+    $('form > input[name=email]').should.not.be.checked;
   }
 };

bb1ee68ebef070343bf13b70b826ee3e667641d5
Tj Holowaychuk <tj@vision-media.ca>
2010-12-07 11:10:17 -0800

    More jQuery#fill() tests

--- test/jquery.test.js
+++ test/jquery.test.js
@@ -13,24 +13,38 @@ function jquery(html) {
 module.exports = {
   'test .fill()': function(){
     var $ = jquery('<form>'
-      + '<input type="text" name="username" />'
+      + '<input id="field-username" type="text" name="username" />'
       + '<input type="password" name="password" />'
       + '<input type="radio" name="signature" value="display" />'
       + '<input type="radio" name="signature" value="hide" />'
       + '<input type="checkbox" name="agreement" value="ageed" />'
       + '<input type="checkbox" name="email" value="yes" checked="checked" />'
+      + '<select name="province">'
+      + '  <option value="bc">British Columbia</option>'
+      + '  <option value="ab">Alberta</option>'
+      + '</select>'
       + '</form>');
 
     $('form').fill({
-        username: 'tjholowaychuk'
+        'field-username': 'tjholowaychuk'
       , signature: 'display'
       , agreement: true
       , email: false
+      , province: 'bc'
     });
 
     $('form > input[name=username]').should.have.attr('value', 'tjholowaychuk');
     $('form > input[value=display]').should.be.checked;
     $('form > input[name=agreement]').should.be.checked;
     $('form > input[name=email]').should.not.be.checked;
+    $('select > option[value=bc]').should.be.selected;
+    $('select > option[value=ab]').should.not.be.selected;
+    
+    $('form').fill({
+      province: ['ab', 'bc']
+    });
+    
+    $('select > option[value=bc]').should.be.selected;
+    $('select > option[value=ab]').should.be.selected;
   }
 };

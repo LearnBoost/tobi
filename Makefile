249bb22e1c03bd6085f74ba6c0d785770b96ef27
Tj Holowaychuk <tj@vision-media.ca>
2010-12-07 14:17:56 -0800

    Added first context test

--- Makefile
+++ Makefile
@@ -4,6 +4,7 @@ TESTS = test/*.test.js
 test:
 	@./support/expresso/bin/expresso \
 		--serial \
+		--timeout 5000 \
 		-I support \
 		-I support/jsdom/lib \
 		-I support/htmlparser/lib \
--- test/browser.context.test.js
+++ test/browser.context.test.js
@@ -16,10 +16,10 @@ app.use(express.bodyDecoder());
 
 app.get('/search', function(req, res){
   res.send(
-      '<form id="user-search" action="/search/users">'
+      '<form id="user-search" method="post" action="/search/users">'
     + '  <input type="text" name="query" />'
     + '</form>'
-    + '<form id="post-search" action="/search/posts">'
+    + '<form id="post-search" method="post" action="/search/posts">'
     + '  <input type="text" name="query" />'
     + '</form>');
 });
@@ -32,9 +32,20 @@ app.post('/search/posts', function(req, res){
   res.send({ posts: true, headers: req.headers, body: req.body });
 });
 
+var browser = tobi.createBrowser(app);
+
 module.exports = {
-  'test ': function(){
-    
+  'test global context': function(done){
+    browser.get('/search', function($){
+      $('form').should.have.length(2);
+      browser
+      .type('query', 'foo bar')
+      .submit('user-search', function(res){
+        res.body.users.should.be.true;
+        res.body.body.should.eql({ query: 'foo bar' });
+        done();
+      });
+    });
   },
   
   after: function(){

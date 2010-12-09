aa1ae3f00128031f6b150a319f6b4c1add34714e
Tj Holowaychuk <tj@vision-media.ca>
2010-12-09 13:55:48 -0800

    Fixed wizard example

--- examples/wizard.js
+++ examples/wizard.js
@@ -23,7 +23,7 @@ browser.get('/wizard', function(res, $){
       res.should.have.status(200);
       $('h1').should.have.text('Details');
       $('form')
-        .select('city', 'Victoria')
+        .fill({ city: 'Victoria' })
         .find('[value=Continue]')
         .click(function(res, $){
           res.should.have.status(200);
@@ -31,13 +31,14 @@ browser.get('/wizard', function(res, $){
           $(':submit').should.have.value('Complete');
           $('ul li:nth-child(1)').should.have.text('Name: tj');
           $('ul li:nth-child(2)').should.have.text('Email: tj@learnboost.com');
-          $('ul li:nth-child(3)').should.have.text('City: Victoria');
+          $('ul li:nth-child(3)').should.have.text('City: victoria');
           $('form').submit(function(res, $){
             res.should.have.status(200);
             $('h1').should.have.text('Registration Complete');
             $('ul li:nth-child(1)').should.have.text('Name: tj');
             $('ul li:nth-child(2)').should.have.text('Email: tj@learnboost.com');
-            $('ul li:nth-child(3)').should.have.text('City: Victoria');
+            $('ul li:nth-child(3)').should.have.text('City: victoria');
+            console.log('successful');
             app.close();
           });
         });

da4eb6d8fb48814eae10c15fb91e22642751da31
Tj Holowaychuk <tj@vision-media.ca>
2010-12-13 11:31:02 -0800

    Added failing jsdom test

--- test/browser.external.test.js
+++ test/browser.external.test.js
@@ -14,7 +14,16 @@ var app = express.createServer();
 
 app.use(express.bodyDecoder());
 
-app.get('/script', function(req, res, next){
+app.get('/remote/script.js', function(req, res){
+  res.header('Content-Type', 'application/javascript');
+  res.send('document.getElementById("para").innerHTML = "<em>new</em>";');
+});
+
+app.get('/remote', function(req, res){
+  res.send('<html><head><script src="/remote/script.js"></script></head><body><p>old</p></body></html>')
+});
+
+app.get('/script', function(req, res){
   res.send('<script>document.getElementById("para").innerHTML = "<em>new</em>";</script><p id="para">old</p>');
 });
 
@@ -31,6 +40,15 @@ module.exports = {
   'test external option enabled': function(done){
     var browser = tobi.createBrowser(app, { external: true });
     browser.get('/script', function(res, $){
+      res.should.have.status(200);
+      $('p').should.have.text('new');
+      done();
+    });
+  },
+  
+  'test remote scripts': function(done){
+    var browser = tobi.createBrowser(app, { external: true });
+    browser.get('/remote', function(res, $){
       res.should.have.status(200);
       $('p').should.have.text('new');
       done();

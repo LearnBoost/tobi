8865cdc8c0519589d6a95fcd50ee3c7a48e5177e
Mike Hemesath <mike.hemesath@gmail.com>
2011-05-16 07:35:49 -0700

    The example seemed to just hang so I modified the url to 'www.google.com' and it seems to execute as expected now.

--- examples/google.js
+++ examples/google.js
@@ -1,11 +1,10 @@
-
 /**
  * Module dependencies.
  */
 
 var tobi = require('../')
   , should = require('should')
-  , browser = tobi.createBrowser(80, 'google.com');
+  , browser = tobi.createBrowser(80, 'www.google.com');
 
 browser.get('/', function(res){
   res.should.have.status(200);
